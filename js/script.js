4aa9b62ff9ee02915707b452c4194c910edac4c2
Joseph Doucet <sssd09@gmail.com>
2021-01-07 14:33:13 -0500

    Add more psuedocode instructions

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 //Set url to api
 const randomUser = fetch('https://randomuser.me/api/?results=12')
+//Retrive api info
     .then(res => console.log(res.json()))
 
 
-//Retrive api info
-//
+//Generate search field to filter out random users already on the page (only in english)
+//Generate html cards for each random user and append them to '.card' div
+//Generate the modal window for each card as it is clicked
+//Add functionality for the next and previous arrow keys
+
+
+//Event listeners
