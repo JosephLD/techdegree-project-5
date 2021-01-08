87284ac8b79aa5031b13c29e7feaaf6e91c9b8ea
Joseph Doucet <sssd09@gmail.com>
2021-01-08 15:27:04 -0500

    update api url

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 //Set url to api
-const randomUser = fetch('https://randomuser.me/api/?results=12')
 //Retrive api info
-    .then(res => console.log(res.json()))
-
+const randomUser = fetch('https://randomuser.me/api/?results=12&nat=gb,au')
+    .then(res => res.json())
+    .then(promise => console.log(promise.results))
+//Set the .card div to card
+const card = document.querySelector('.card')
 
 //Generate search field to filter out random users already on the page (only in english)
 //Generate html cards for each random user and append them to '.card' div
+
 //Generate the modal window for each card as it is clicked
 //Add functionality for the next and previous arrow keys
 
